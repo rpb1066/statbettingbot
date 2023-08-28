e1bb661773e0fd1d5df3df99656c45c9b9f18a20
Ryan Blackledge <rpb1066@googlemail.com>
2023-08-29 00:25:10 +0100

    Converted distanceinfo.py to class

--- distanceinfo.py
+++ distanceinfo.py
@@ -1,55 +1,54 @@
 import math
 import csv
+class DistanceInfo:
+    def __init__(self):
+        self.team_location = dict()
+        self.time_taken_dict = dict()
 
-team_location = dict()
-time_taken_dict = dict()
-def get_team_dict():
-    team_location["arsenal"] = "London"
-    team_location["aston villa"] = "Birmingham"
-    team_location["bournemouth afc"] = "Bournemouth"
-    team_location["brentford"] = "London"
-    team_location["brighton & hove albion"] = "Brighton"
-    team_location["burnley"] = "Burnley"
-    team_location["chelsea"] = "London"
-    team_location["crystal palace"] = "London"
-    team_location["everton"] = "Liverpool"
-    team_location["fulham"] = "London"
-    team_location["liverpool"] = "Liverpool"
-    team_location["luton town"] = "Luton"
-    team_location["manchester city"] = "Manchester"
-    team_location["manchester united"] = "Manchester"
-    team_location["newcastle united"] = "Newcastle"
-    team_location["nottingham forest"] = "Nottingham"
-    team_location["southampton"] = "Southampton"
-    team_location["tottenham hotspur"] = "London"
-    team_location["west ham united"] = "London"
-    team_location["wolverhampton wanderers"] = "Wolverhampton"
+        self.create_team_dict()
+        self.create_time_travelled_dict()
 
-def create_time_travelled_dict():
-    file = open("Resources/traveltimes.txt","r")
-    time_taken_array = file.read().split("\n")
-    for timepair in time_taken_array:
-        locA,locB,time = timepair.split(",")
-        time_taken_dict[frozenset((locA,locB))] = time
+    def create_team_dict(self):
+        self.team_location["arsenal"] = "London"
+        self.team_location["aston villa"] = "Birmingham"
+        self.team_location["bournemouth afc"] = "Bournemouth"
+        self.team_location["brentford"] = "London"
+        self.team_location["brighton & hove albion"] = "Brighton"
+        self.team_location["burnley"] = "Burnley"
+        self.team_location["chelsea"] = "London"
+        self.team_location["crystal palace"] = "London"
+        self.team_location["everton"] = "Liverpool"
+        self.team_location["fulham"] = "London"
+        self.team_location["liverpool"] = "Liverpool"
+        self.team_location["luton town"] = "Luton"
+        self.team_location["manchester city"] = "Manchester"
+        self.team_location["manchester united"] = "Manchester"
+        self.team_location["newcastle"] = "Newcastle"
+        self.team_location["nottingham forest"] = "Nottingham"
+        self.team_location["southampton"] = "Southampton"
+        self.team_location["tottenham hotspur"] = "London"
+        self.team_location["west ham united"] = "London"
+        self.team_location["wolverhampton wanderers"] = "Wolverhampton"
 
+    def create_time_travelled_dict(self):
+        file = open("Resources/traveltimes.txt", "r")
+        time_taken_array = file.read().split("\n")
+        for timepair in time_taken_array:
+            locA, locB, time = timepair.split(",")
+            self.time_taken_dict[frozenset((locA, locB))] = time
 
-def get_distance_between_two_teams(A, B):
-    team_a = A.lower()
-    team_b = B.lower()
-    if team_location[team_a] == team_location[team_b]:
-        return 0
-    else:
-        return time_taken_dict[frozenset((team_location[team_a].lower(),team_location[team_b].lower()))]
+    def get_distance_between_two_teams(self, A, B):
+        team_a = A.lower()
+        team_b = B.lower()
 
+        for team in self.team_location.keys():
+            if team_a in team:
+                team_a = team
+            if team_b in team:
+                team_b = team
 
-def init_distance_info():
-     get_team_dict()
-     create_time_travelled_dict()
-
-
-
-
-
-
-
-
+        if self.team_location[team_a] == self.team_location[team_b]:
+            return 0
+        else:
+            return self.time_taken_dict[
+                frozenset((self.team_location[team_a].lower(), self.team_location[team_b].lower()))]
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import distanceinfo
 import distanceinfo as di
 
 if __name__ == '__main__':
-    distanceinfo.init_distance_info()
-    print(di.get_distance_between_two_teams("Liverpool", "Manchester United"))
+    distance_info = di.DistanceInfo()
+    print(distance_info.get_distance_between_two_teams("west ham", "liverpool"))
 
